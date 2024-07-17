1227119560a116ce0ff4e96918c7c5756ab4d3e5
AprilNEA <github@sku.moe>
2024-07-17 21:12:07 +0800

    chore: uncomment noused code

--- src/app/(non-public)/device/modules/PastLogTable.tsx
+++ src/app/(non-public)/device/modules/PastLogTable.tsx
@@ -17,34 +17,35 @@ import { Button } from "@/components/ui/button";
 const PastLogTable: React.FC<{
   device_id: string;
 }> = ({ device_id }) => {
-  const { data } = api.devicePastLogList.useSWR({ device_id });
-
-  return (
-    <Table>
-      <TableCaption>A list of your recent invoices.</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">Log ID</TableHead>
-          <TableHead>Create Time</TableHead>
-          <TableHead className="text-right">Action</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {data?.map((log) => (
-          <TableRow key={log.id}>
-            <TableCell className="font-medium">{log.id}</TableCell>
-            <TableCell>{log.update_time?.toLocaleDateString()}</TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-      <TableFooter>
-        <TableRow>
-          <TableCell colSpan={3}>Total</TableCell>
-          <TableCell className="text-right">$2,500.00</TableCell>
-        </TableRow>
-      </TableFooter>
-    </Table>
-  );
+  return <></>;
+  // const { data } = api.devicePastLogList.useSWR({ device_id });
+  //
+  // return (
+  //   <Table>
+  //     <TableCaption>A list of your recent invoices.</TableCaption>
+  //     <TableHeader>
+  //       <TableRow>
+  //         <TableHead className="w-[100px]">Log ID</TableHead>
+  //         <TableHead>Create Time</TableHead>
+  //         <TableHead className="text-right">Action</TableHead>
+  //       </TableRow>
+  //     </TableHeader>
+  //     <TableBody>
+  //       {data?.map((log) => (
+  //         <TableRow key={log.id}>
+  //           <TableCell className="font-medium">{log.id}</TableCell>
+  //           <TableCell>{log.update_time?.toLocaleDateString()}</TableCell>
+  //         </TableRow>
+  //       ))}
+  //     </TableBody>
+  //     <TableFooter>
+  //       <TableRow>
+  //         <TableCell colSpan={3}>Total</TableCell>
+  //         <TableCell className="text-right">$2,500.00</TableCell>
+  //       </TableRow>
+  //     </TableFooter>
+  //   </Table>
+  // );
 };
 
 export default PastLogTable;
