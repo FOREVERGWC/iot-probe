9c94e15ef152839ff2254f93dafa6b6c31a0c458
瑶 <916586595@qq.com>
2024-08-13 18:57:08 +0800

    feat: 数据定时刷新

--- src/app/(non-public)/device/[id]/page.tsx
+++ src/app/(non-public)/device/[id]/page.tsx
@@ -1,12 +1,11 @@
 "use client";
 
+import { useEffect } from "react";
 import { api } from "@/utils/trpc";
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
-  TableFooter,
   TableHead,
   TableHeader,
   TableRow,
@@ -22,9 +21,15 @@ import {
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import Loading from "@/components/loading";
-import {base64Encoded, formattedDate} from "@/utils/time";
+import { base64Encoded, formattedDate } from "@/utils/time";
 import RecordChangeTable from "@/app/(non-public)/device/[id]/modules/RecordChangeTable";
-import {columns, columns2, columns3, columns4, columns5} from "@/app/(non-public)/device/[id]/modules/types";
+import {
+  columns,
+  columns2,
+  columns3,
+  columns4,
+  columns5,
+} from "@/app/(non-public)/device/[id]/modules/types";
 import DataTable from "@/app/(non-public)/device/[id]/modules/DataTable";
 
 const Row: React.FC<{
@@ -32,34 +37,42 @@ const Row: React.FC<{
   children: React.ReactNode;
 }> = ({ label, children }) => {
   return (
-    <div className="flex flex-row justify-between">
-      <span className="pr-2 font-medium">{label}</span>
-      <span>{children}</span>
-    </div>
+      <div className="flex flex-row justify-between">
+        <span className="pr-2 font-medium">{label}</span>
+        <span>{children}</span>
+      </div>
   );
 };
 
 const MsSpeed = ({ speed }: { speed: string }) => (
-  <>
-    {speed}
-    {parseInt(speed.slice(0, -2)) > 9000 && (
-      <Badge variant="destructive" className="mx-2">
-        离线
-      </Badge>
-    )}
-  </>
+    <>
+      {speed}
+      {parseInt(speed.slice(0, -2)) > 9000 && (
+          <Badge variant="destructive" className="mx-2">
+            离线
+          </Badge>
+      )}
+    </>
 );
 
 export default function Page({ params }: { params: { id: string } }) {
-  const { data } = api.device.useSWR({ id: params.id });
+  const { data, mutate } = api.device.useSWR({ id: params.id });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      mutate();
+    }, 60000);
+
+    return () => clearInterval(interval);
+  }, [mutate]);
 
   if (!data) {
     return <Loading />;
   }
 
   return (
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-10">
-        <Card className="col-span-2 row-span-2">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-4 md:p-10">
+        <Card className="col-span-1 md:col-span-2 lg:col-span-2 row-span-2">
           <CardHeader>
             <CardTitle>探针</CardTitle>
             <CardDescription>{data.device.device_id}</CardDescription>
@@ -67,20 +80,35 @@ export default function Page({ params }: { params: { id: string } }) {
           <CardContent className="space-y-2">
             <Row label="状态">
               <Badge
-                  className={data.device.is_online ? "bg-green-500" : "bg-red-500"}
+                  className={
+                    data.device.is_online ? "bg-green-500" : "bg-red-500"
+                  }
               >
                 {data.device.is_online ? "在线" : "离线"}
               </Badge>
             </Row>
             <Row label="电源状态">
               <Badge
-                  className={data.device.is_online ? (data.lastLog?.electric === 'correct' ? "bg-green-500" : "bg-red-500") : "bg-gray-500"}>
-                {data.device.is_online ? (data.lastLog?.electric === 'correct' ? '正常' : '断电') : '未知'}
+                  className={
+                    data.device.is_online
+                        ? data.lastLog?.electric === "correct"
+                            ? "bg-green-500"
+                            : "bg-red-500"
+                        : "bg-gray-500"
+                  }
+              >
+                {data.device.is_online
+                    ? data.lastLog?.electric === "correct"
+                        ? "正常"
+                        : "断电"
+                    : "未知"}
               </Badge>
             </Row>
             <Row label="信号强度">{data.lastLog.signal_quality_rssi}</Row>
             <Row label="固件版本">{data.lastLog.firmware_version}</Row>
-            <Row label="最后日志 ID">{data.device.latest_device_log_id}</Row>
+            <Row label="最后日志 ID">
+              {data.device.latest_device_log_id}
+            </Row>
             <Row label="最后上线于">
               {formattedDate(data.lastLog.update_time)}
             </Row>
@@ -89,41 +117,65 @@ export default function Page({ params }: { params: { id: string } }) {
             <Button>查看历史日志</Button>
           </CardFooter>
         </Card>
-        <Card className="col-span-1 row-span-1">
+        <Card className="col-span-1">
           <CardHeader>
             <CardTitle>有线网络</CardTitle>
-            <Row label="设备 IP">{data.device.intranet_array.split(",")[0]}</Row>
-            <Row label="网关 IP">{data.device.intranet_array.split(",")[1]}</Row>
-            <Row
-                label="以太网状态">{data.lastChangeLog?.ethernet === -1 ? '正常' : (data.lastChangeLog?.ethernet ? '异常' : '未知')}</Row>
+            <Row label="设备 IP">
+              {data.device.intranet_array.split(",")[0]}
+            </Row>
+            <Row label="网关 IP">
+              {data.device.intranet_array.split(",")[1]}
+            </Row>
+            <Row label="以太网状态">
+              {data.lastChangeLog?.ethernet === -1
+                  ? "正常"
+                  : data.lastChangeLog?.ethernet
+                      ? "异常"
+                      : "未知"}
+            </Row>
           </CardHeader>
         </Card>
-        <Card className="col-span-1 row-span-1">
+        <Card className="col-span-1">
           <CardHeader>
             <CardTitle>电源状态</CardTitle>
             <Row label="电源状态">
               <Badge
-                  className={data.device.is_online ? (data.lastLog?.electric === 'correct' ? "bg-green-500" : "bg-red-500") : "bg-gray-500"}>
-                {data.device.is_online ? (data.lastLog?.electric === 'correct' ? '正常' : '断电') : '未知'}
+                  className={
+                    data.device.is_online
+                        ? data.lastLog?.electric === "correct"
+                            ? "bg-green-500"
+                            : "bg-red-500"
+                        : "bg-gray-500"
+                  }
+              >
+                {data.device.is_online
+                    ? data.lastLog?.electric === "correct"
+                        ? "正常"
+                        : "断电"
+                    : "未知"}
               </Badge>
             </Row>
           </CardHeader>
         </Card>
-        <Card className="col-span-1 row-span-1">
+        <Card className="col-span-1">
           <CardHeader>
             <CardTitle>SIM 卡状态</CardTitle>
             <Row label="ICCID">{data.lastLog.iccid}</Row>
           </CardHeader>
         </Card>
-        <Card className="col-span-1 row-span-1">
+        <Card className="col-span-1">
           <CardHeader>
             <CardTitle>最新消息</CardTitle>
-            <Row label="接收数据">{base64Encoded(data.lastLog.serial_tx)}</Row>
-            <Row label="发送数据">{base64Encoded(data.lastLog.serial_rx)}</Row>
+            <Row label="接收数据">
+              {base64Encoded(data.lastLog.serial_tx)}
+            </Row>
+            <Row label="发送数据">
+              {base64Encoded(data.lastLog.serial_rx)}
+            </Row>
           </CardHeader>
         </Card>
-        <div className="col-span-4 grid grid-cols-1 md:grid-cols-2 gap-4">
-          <Card className="col-span-1 md:col-span-1 row-span-2">
+        <div className="col-span-1 md:col-span-2 lg:col-span-4 grid grid-cols-1 md:grid-cols-2 gap-4">
+          <Card className="row-span-2">
             <CardHeader>
               <CardTitle>内网状态</CardTitle>
               <CardDescription>内网探针记录</CardDescription>
@@ -140,7 +192,9 @@ export default function Page({ params }: { params: { id: string } }) {
                 <TableBody>
                   {data.lastLog.intranet_ping_array &&
                       (
-                          JSON.parse(data.lastLog.intranet_ping_array) as unknown as {
+                          JSON.parse(
+                              data.lastLog.intranet_ping_array
+                          ) as unknown as {
                             intranetIP: string;
                             speed: string;
                             speedLarge: string;
@@ -151,10 +205,10 @@ export default function Page({ params }: { params: { id: string } }) {
                               {ping.intranetIP}
                             </TableCell>
                             <TableCell>
-                              <MsSpeed speed={ping.speed}/>
+                              <MsSpeed speed={ping.speed} />
                             </TableCell>
                             <TableCell>
-                              <MsSpeed speed={ping.speedLarge}/>
+                              <MsSpeed speed={ping.speedLarge} />
                             </TableCell>
                           </TableRow>
                       ))}
@@ -165,17 +219,17 @@ export default function Page({ params }: { params: { id: string } }) {
               <Button>添加内针探针</Button>
             </CardFooter>
           </Card>
-          <Card className="col-span-1 md:col-span-1 row-span-2">
+          <Card className="row-span-2">
             <CardHeader>
               <CardTitle>外网状态</CardTitle>
               <CardDescription>外网探针记录</CardDescription>
             </CardHeader>
             <CardContent className="space-y-2">
               <Row label="外网 Ping">
-                <MsSpeed speed={data.lastLog.extranet_ping!}/>
+                <MsSpeed speed={data.lastLog.extranet_ping!} />
               </Row>
               <Row label="外网 Ping Large">
-                <MsSpeed speed={data.lastLog.extranet_ping_large!}/>
+                <MsSpeed speed={data.lastLog.extranet_ping_large!} />
               </Row>
             </CardContent>
             <CardFooter>
@@ -183,14 +237,34 @@ export default function Page({ params }: { params: { id: string } }) {
             </CardFooter>
           </Card>
         </div>
-        <div className="col-span-4 grid grid-cols-1 md:grid-cols-3 gap-4">
-          <RecordChangeTable title="设备状态" device_id={params.id} columns={columns}/>
-          <RecordChangeTable title="电源状态" device_id={params.id} columns={columns2}/>
-          <RecordChangeTable title="以太网状态" device_id={params.id} columns={columns3}/>
+        <div className="col-span-1 md:col-span-2 lg:col-span-4 grid grid-cols-1 md:grid-cols-3 gap-4">
+          <RecordChangeTable
+              title="设备状态"
+              device_id={params.id}
+              columns={columns}
+          />
+          <RecordChangeTable
+              title="电源状态"
+              device_id={params.id}
+              columns={columns2}
+          />
+          <RecordChangeTable
+              title="以太网状态"
+              device_id={params.id}
+              columns={columns3}
+          />
         </div>
-        <div className="col-span-4 grid grid-cols-1 md:grid-cols-2 gap-4">
-          <DataTable title="接收数据" device_id={params.id} columns={columns5}/>
-          <DataTable title="发送数据" device_id={params.id} columns={columns4}/>
+        <div className="col-span-1 md:col-span-2 lg:col-span-4 grid grid-cols-1 md:grid-cols-2 gap-4">
+          <DataTable
+              title="接收数据"
+              device_id={params.id}
+              columns={columns5}
+          />
+          <DataTable
+              title="发送数据"
+              device_id={params.id}
+              columns={columns4}
+          />
         </div>
       </div>
   );
--- src/app/(non-public)/page.tsx
+++ src/app/(non-public)/page.tsx
@@ -1,87 +1,91 @@
 "use client";
 
-import { api } from "@/utils/trpc";
+import {useEffect} from "react";
+import {api} from "@/utils/trpc";
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
 } from "@/components/ui/card";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Badge } from "@/components/ui/badge";
+import {Badge} from "@/components/ui/badge";
 import Link from "next/link";
 import Loading from "@/components/loading";
 
 const Row: React.FC<{
-  label: string;
-  children: React.ReactNode;
-}> = ({ label, children }) => {
-  return (
-    <div className="flex flex-row justify-between">
-      <span className="pr-2 font-medium">{label}</span>
-      {children}
-    </div>
-  );
+    label: string;
+    children: React.ReactNode;
+}> = ({label, children}) => {
+    return (
+        <div className="flex flex-row justify-between">
+            <span className="pr-2 font-medium">{label}</span>
+            {children}
+        </div>
+    );
 };
 
 export default function Home() {
-  const { data } = api.devices.useSWR();
+    const { data, mutate} = api.devices.useSWR();
 
-  if (!data) {
-    return <Loading />;
-  }
+    useEffect(() => {
+        const interval = setInterval(() => {
+            mutate();
+        }, 60000);
 
-  return (
-    <main className="grid min-h-screen grid-cols-1 base:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-24 mx-auto">
-      {data.map((device: any) => (
-        <Link key={device.device_id} href={`/device/${device.device_id}`}>
-          <Card className="w-72">
-            <CardHeader>
-              <CardTitle className="truncate">{device.device_id}</CardTitle>
-              <CardDescription></CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-2">
-              <Row label="状态">
-                <Badge
-                  className={device.is_online ? "bg-green-500" : "bg-red-500"}
-                >
-                  {device.is_online ? "在线" : "离线"}
-                </Badge>
-              </Row>
-              <Row label="电源状态">
-                <Badge
-                    className={device.is_online ? (device.device_log?.electric === 'correct' ? "bg-green-500" : "bg-red-500") : "bg-gray-500"}>
-                  {device.is_online ? (device.device_log?.electric === 'correct' ? '正常' : '断电') : '未知'}
-                </Badge>
-              </Row>
-              <Row label="信号强度">{device.device_log?.signal_quality_rssi}</Row>
-              <Row label="固件版本">{device.device_log?.firmware_version}</Row>
-              <Dialog>
-                <DialogTrigger>
-                  <Row label="最后日志 ID">{device.latest_device_log_id}</Row>
-                </DialogTrigger>
-                <DialogContent>
-                  <DialogHeader>
-                    <DialogTitle>Are you absolutely sure?</DialogTitle>
-                    <DialogDescription>
-                      This action cannot be undone. This will permanently delete
-                      your account and remove your data from our servers.
-                    </DialogDescription>
-                  </DialogHeader>
-                </DialogContent>
-              </Dialog>
-            </CardContent>
-          </Card>
-        </Link>
-      ))}
-    </main>
-  );
+        return () => clearInterval(interval);
+    }, [mutate]);
+
+    if (!data) {
+        return <Loading/>;
+    }
+
+    return (
+        <main
+            className="grid min-h-screen grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-12 mx-auto">
+            {data.map((device: any) => (
+                <Card key={device.device_id} className="w-full max-w-sm mx-auto">
+                    <Link href={`/device/${device.device_id}`}>
+                        <CardHeader>
+                            <CardTitle className="truncate">{device.device_id}</CardTitle>
+                            <CardDescription></CardDescription>
+                        </CardHeader>
+                        <CardContent className="space-y-2">
+                            <Row label="状态">
+                                <Badge
+                                    className={device.is_online ? "bg-green-500" : "bg-red-500"}
+                                >
+                                    {device.is_online ? "在线" : "离线"}
+                                </Badge>
+                            </Row>
+                            <Row label="电源状态">
+                                <Badge
+                                    className={
+                                        device.is_online
+                                            ? device.device_log?.electric === "correct"
+                                                ? "bg-green-500"
+                                                : "bg-red-500"
+                                            : "bg-gray-500"
+                                    }
+                                >
+                                    {device.is_online
+                                        ? device.device_log?.electric === "correct"
+                                            ? "正常"
+                                            : "断电"
+                                        : "未知"}
+                                </Badge>
+                            </Row>
+                            <Row label="信号强度">
+                                {device.device_log?.signal_quality_rssi}
+                            </Row>
+                            <Row label="固件版本">
+                                {device.device_log?.firmware_version}
+                            </Row>
+                            <Row label="最后日志 ID">{device.latest_device_log_id}</Row>
+                        </CardContent>
+                    </Link>
+                </Card>
+            ))}
+        </main>
+    );
 }
